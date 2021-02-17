c5b56f505459a3fe161c48a008dfce700ebf8826
mnstucky <mnstucky@gmail.com>
2021-02-16 21:01:04 -0600

    Create ConvertStringToNumber.cs

---
+++ src/ConvertStringToNumber.cs
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/544675c6f971f7399a000e79/train/csharp
+
+using System;
+  public class Kata
+  {
+    public static int StringToNumber(String str) {
+        return Int32.Parse(str);
+  }
+}
